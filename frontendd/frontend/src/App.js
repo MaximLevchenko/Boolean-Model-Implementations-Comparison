56cf3da88a5c1936ce558db3b5f9edc739d1b136
levchmak <levchmak@fit.cvut.cz>
2024-07-30 22:13:09 +0200

    frontend

---
+++ frontendd/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search from './components/Search'; // Adjust the path as necessary
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
